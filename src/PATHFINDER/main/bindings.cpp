100f278f8c619355ee36b0c8286eb4a9227812c1
FF-Flori <ffilkohazi3@gmail.com>
2026-02-24 21:03:29 +0100

    Added simple list loading

--- src/PATHFINDER/main/bindings.cpp
+++ src/PATHFINDER/main/bindings.cpp
@@ -1,2 +1,13 @@
 #include "pathfinder.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
+namespace py = pybind11;
+
+PYBIND11_MODULE(pathfinder, m) {
+    py::class_<Pathfinder>(m, "Pathfinder")
+        .def(py::init<>())
+        .def("start", [](Pathfinder &self, const std::vector<std::vector<char>>& data) {
+            return self.inputTransformation(data);
+        });
+}
--- src/PATHFINDER/main/pathfinder.hpp
+++ src/PATHFINDER/main/pathfinder.hpp
@@ -1,9 +1,16 @@
 #ifndef VD26_PATHFINDER_HPP
 #define VD26_PATHFINDER_HPP
+#include <vector>
 
 
 class Pathfinder {
-
+	private:
+		int startPos[2] = {0,0};
+		char* table;
+	public:
+		Pathfinder();
+		// Mivel gondolom hogy sorok listája lesz majd az input
+		void inputTransformation(std::vector<std::vector<char>> inputList);
 };
 
 
