6ffabb454d2a5df50d27482f80710849c022be1e
Hegedűs István András <hegedus.i.andris@gmail.com>
2026-02-24 09:24:23 +0100

    map tiles enum

--- src/PATHFINDER/main/pathfinder.hpp
+++ src/PATHFINDER/main/pathfinder.hpp
@@ -1,6 +1,14 @@
 #ifndef VD26_PATHFINDER_HPP
 #define VD26_PATHFINDER_HPP
 
+enum class tile_t : uint8_t {
+    start,
+    land,
+    wall,
+    blue,
+    yellow,
+    green
+};
 
 class Pathfinder {
 
