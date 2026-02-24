e03f1d98f43d8f0779dd01895459d433ba05664a
FF-Flori <ffilkohazi3@gmail.com>
2026-02-24 06:46:20 +0100

    added more things to the game subclass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.GUI import MainWindow as game
 
-window:game.MainWindow = game.MainWindow(800,600,"GAME")
+window:game.MainWindow = game.MainWindow(700,900,"GAME")
 
 window.mainloop()
 window.quit()
--- src/GAME/game.py
+++ src/GAME/game.py
@@ -1,9 +1,11 @@
 import pygame
-
+from src.GAME.gamepanel import *
 
 class Game:
     def __init__(self) -> None:
-        print("Initialized")
+        self.gameHandler:GamePanel = GamePanel()
     def drawGame(self, screen:pygame.Surface) -> None:
         # Ide kerül majd minden rajzolás a game subclassaiból
-        screen.fill("cyan")
+        screen.fill("gray")
+        self.gameHandler.showPanel(screen)
+
