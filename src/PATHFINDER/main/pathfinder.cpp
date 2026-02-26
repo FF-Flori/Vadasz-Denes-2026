e38d459c2878dea8b6eeda1fc420d3b1ab97ba23
Hegedűs István András <hegedus.i.andris@gmail.com>
2026-02-26 20:24:21 +0100

    sadly removed input transformer function (got replaced by the file reading in the constructor)

--- src/PATHFINDER/main/pathfinder.cpp
+++ src/PATHFINDER/main/pathfinder.cpp
@@ -46,27 +46,3 @@ Pathfinder::Pathfinder(const uint16_t timeLimit, const std::string& mapPath) : t
 
 	file.close();
 }
-
-
-//Csak akkor fog működni hogy ha mindegyik sor ugyan olyan hosszú
-void Pathfinder::inputTransformation(std::vector<std::vector<char>> inputList){
-	std::cout<<"Start\n";
-	int tablesize = inputList.size()* inputList.at(0).size();
-	//tudom hogy a sizeof char az csak simán 1 szóval nem számít, de azért bele szoktam tenni
-	table = (char*)malloc(sizeof(char)*(tablesize+1));
-	width = inputList.at(0).size();
-	height = inputList.size();
-
-	int counter = 0;
-	for(std::vector<char> line : inputList){
-		for(char currchar : line){
-			assert(counter < tablesize);
-			table[counter] = currchar;
-			counter++;
-		}
-	}
-	table[counter] = '\0';
-	std::cout<<table<<"\n";
-	return;
-}
-
