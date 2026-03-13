bf16063f3da21ebb5c2caae0700190f97c7e8643
Gamer Ice Play Bence <gamerbence90@gmail.com>
2026-03-13 13:40:54 +0100

    Final fix

--- dashboard.py
+++ dashboard.py
@@ -120,11 +120,8 @@ class Logexplorer:
                 self.listbox.insert(END, file[:-4])
 
     def done(self, logfile):
-        if logfile != None and len(logfile) > 0:
-            global window
-            window = DashboardUI(self.main, logfile)
-        else:
-            print("Kötelező kijelölni 1 elemet!")
+        global window
+        window = DashboardUI(self.main, logfile)
 
 
 logline:int = 1
@@ -225,40 +222,134 @@ class FullGraphWindow:
         self.ticks = ticks
         self.ticklabels = ticklabels
 
+        if mode == "coords":
+            self.ax.grid(True)
+            self.ax.set_xlim(0, 50)
+            self.ax.set_ylim(0, 50)
+            mars_map = []
+
+            with open("src/PATHFINDER/mars_map_50x50.csv", "r") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    mars_map.append(row)
+
+            grid = []
+
+            start_x = 0
+            start_y = 0
+
+            for y in range(50):
+                grid_row = []
+                for x in range(50):
+                    tile = mars_map[y][x]
+
+                    if tile == "#":
+                        grid_row.append(1)
+
+                    elif tile == "B":
+                        grid_row.append(2)
+
+                    elif tile == "Y":
+                        grid_row.append(3)
+
+                    elif tile == "G":
+                        grid_row.append(4)
+
+                    elif tile == "S":
+
+                        grid_row.append(5)
+
+                        start_x = x
+                        start_y = y
+
+                    else:
+                        grid_row.append(0)
+
+                grid.append(grid_row)
+
+            cmap = ListedColormap([
+                "#2e3237",
+                "gray",
+                "blue",
+                "yellow",
+                "green",
+                "lightgreen"
+            ])
+
+            self.ax.imshow(
+                grid,
+                cmap=cmap,
+                origin="upper",
+                extent=(0,50,50,0)
+            )
+
+            self.rover_coords = self.ax.scatter(start_x, start_y, color="red", s=40)
+
+            self.rover_coordstxt = self.ax.annotate("Rover", (start_x, start_y))
+            self.rover_coordstxt.set_fontweight("bold")
+            self.rover_coordstxt.set_color("black")
+
+            self.ax.scatter(start_x, start_y, color="lightgreen", s=40)
+            starttxt = self.ax.annotate("Kiinduló Pont", (start_x, start_y))
+            starttxt.set_fontweight("bold")
+            starttxt.set_color("lightgreen")
+
+            self.ax.set_aspect("auto")
+            self.ax.invert_yaxis()
+
         self.update_graph()
 
     def update_graph(self):
-        self.ax.clear()
-        if self.mode == "line":
-            self.ax.plot(self.xdata, self.ydata, marker="o")
-        elif self.mode == "bar":
-            self.ax.bar(self.xdata, self.ydata, width=20, linewidth=1, color="lightgreen")
-        self.ax.set_ylabel(self.ylabel, color="white")
-        self.ax.set_xlabel(self.xlabel, color="white")
+        if self.mode != "coords":
+            self.ax.clear()
+            if self.mode == "line":
+                self.ax.plot(self.xdata, self.ydata, marker="o")
+            elif self.mode == "bar":
+                self.ax.bar(self.xdata, self.ydata, width=20, linewidth=1, color="lightgreen")
+            self.ax.set_ylabel(self.ylabel, color="white")
+            self.ax.set_xlabel(self.xlabel, color="white")
 
-        if self.ticks is not None and self.ticklabels is not None:
-            self.ax.set_yticks(self.ticks)
-            self.ax.set_yticklabels(self.ticklabels)
+            if self.ticks is not None and self.ticklabels is not None:
+                self.ax.set_yticks(self.ticks)
+                self.ax.set_yticklabels(self.ticklabels)
 
-        self.ax.set_xticks(self.xdata)
+            self.ax.set_xticks(self.xdata)
 
-        labels = [
-                f"{int(i)//60}:{int(i)%60:02d}"
-                for i in self.xdata
-                ]
+            labels = []
+
+            for i in range(len(self.xdata)):
+                if i % 4  == 0:
+                    labels.append(f"{int(self.xdata[i])//60}:{int(self.xdata[i])%60:02d}")                
+                else:
+                    labels.append("")
+            
+            self.ax.set_xticklabels(labels)
+            self.ax.tick_params("x", rotation=90)
+            self.ax.grid(True, linestyle="--", linewidth=0.5)
+            self.ax.set_facecolor("#262626")
+            self.fg.patch.set_facecolor("#262626")
+            self.canvas.draw_idle()
         
-        self.ax.set_xticklabels(labels)
-        self.ax.grid(True, linestyle="--", linewidth=0.5)
-        self.ax.set_facecolor("#262626")
-        self.fg.patch.set_facecolor("#262626")
-        self.canvas.draw_idle()
+        else:
+            self.ax.set_ylabel(self.ylabel, color="white")
+            self.ax.set_xlabel(self.xlabel, color="white")
+            self.rover_coords.set_offsets([self.xdata, self.ydata])
+            self.rover_coordstxt.set_position([self.xdata, self.ydata])
+            self.canvas.draw_idle()
+            
 
 class DashboardUI:
     def __init__(self, main:Ctk.CTk, selectedlogfile:str):
         self.main:Ctk.CTk = main
         self.selectedlogfile:str = selectedlogfile
-
+        
         clearwidget(self.main)
+
+        if selectedlogfile == "log/None.log":
+            global window
+            window = Logexplorer(self.main, "log")
+            return
+
 
         self.main.title("Dashboard")
         self.main.geometry("1400x900")
@@ -311,6 +402,9 @@ class DashboardUI:
 
             elif graph_type == 'speed':
                 self.fullgraph_speed = FullGraphWindow(self.main, "Sebesség", time, speed, "Sebesség", "Idő (óra:perc)", mode="line", ticks=[0,1,2,3], ticklabels=["Áll","Lassú","Normál","Gyors"])
+
+            elif graph_type == "coords":
+                self.fullgraph_coords = FullGraphWindow(self.main, "Rover poziciója", positionX, positionY, "Y", "X", mode="coords")
 
         def create_panel(parent, title, row, col, colspan=1, rfbtn=False, BG=BG2, typein=""):
             outer = CTkFrame(
@@ -400,6 +494,8 @@ class DashboardUI:
             self.bodyframe,
             "Rover Pozíciója",
             1, 1,
+            rfbtn=True,
+            typein='coords',
             BG=BG2
         )
 
