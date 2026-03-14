d00c16fb7804468755099a26cb3b5c6b4b594438
GamerIcePlayBence <gamerbence90@gmail.com>
2026-03-15 00:25:12 +0100

    Create battery display and small fix to time display

--- src/GAME/HUD.py
+++ src/GAME/HUD.py
@@ -15,7 +15,8 @@ class HUD:
         self.yellowmaterial:int = 0
         self.time:int = 0 #half hours
         self.rtime:int = 60 #IDK It dummy
-        
+        self.battery:int = 100 #without %
+
     def show(self, screen:pygame.Surface)->None:
         # Aspect ratio is handled already so we dont need to check the height
         screen.blit(self.img,(0,0))
@@ -25,6 +26,7 @@ class HUD:
         oreswidth:int = 64
         fontcolor = (150, 150, 150)
         panelcolors = (50, 50, 50)
+        batteryimg = pygame.image.load("./src/img/battery/battery7.png").convert_alpha()
 
         pygame.draw.rect(screen, (70, 70, 70), pygame.Rect(self.leftmargin, self.topmargin, self.width, self.height), border_radius=5)
         pygame.draw.rect(screen, panelcolors, pygame.Rect(self.leftmargin+10, self.topmargin+5, 120, self.height-10), border_radius=5)
@@ -48,7 +50,7 @@ class HUD:
         screen.blit(yellowmaterialtxt, ((self.leftmargin+20)+65, (self.topmargin+120)+15))
 
         #Time
-        pygame.draw.rect(screen, panelcolors, pygame.Rect(self.leftmargin+150, self.topmargin+5, self.width-260, 40), border_radius=5)
+        pygame.draw.rect(screen, panelcolors, pygame.Rect(self.leftmargin+150, self.topmargin+5, self.width-245, 40), border_radius=5)
 
         timetxt = font.render(f"{self.time//60:02d}:{self.time%60:02d}", True, fontcolor)
         screen.blit(timetxt, ((self.leftmargin+150)+10, (self.topmargin+5)+10))
@@ -59,3 +61,12 @@ class HUD:
         #IT'S DUMMY
         rtimetxt = font.render(f"Hátralévő: {self.rtime//60:02d}:{self.time%60:02d}", True, fontcolor)
         screen.blit(rtimetxt, (((self.leftmargin+150)+self.width-490)+10, (self.topmargin+5)+10))
+
+        #Battery
+        pygame.draw.rect(screen, panelcolors, pygame.Rect(self.width-50, self.topmargin+5, 70, self.height-10), border_radius=5)
+        
+        batteryimg = pygame.image.load(f"./src/img/battery/battery{int((self.battery / 100) * 7)}.png").convert_alpha()
+        batteryimg = pygame.transform.scale(batteryimg,(60, self.height-50))
+        screen.blit(batteryimg, (self.width-45, self.topmargin+10), pygame.Rect(0,0, 60, self.height-50))
+        batterytxt = font.render(f"{self.battery}%", True, fontcolor)
+        screen.blit(batterytxt, (self.width-45, (self.topmargin+10)+145))
