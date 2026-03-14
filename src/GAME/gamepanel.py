199b04e74f76ef29664b187a5eafba6bf99f2e0c
FF-Flori <ffilkohazi3@gmail.com>
2026-03-14 22:50:47 +0100

    fixed issue with gamepanel overlapping the hud bg and added north facing rover animations

--- src/GAME/gamepanel.py
+++ src/GAME/gamepanel.py
@@ -1,15 +1,18 @@
 import pygame
 from src.GAME.gamelogic import *
+from math import ceil
 
 class GamePanel:
     def __init__(self,windowsize:tuple[int,int])->None:
-        self.margin:int = 20
-        self.width:int = windowsize[0]-2*self.margin
-        self.height:int = windowsize[0]-2*self.margin
-        self.posOnScreen:tuple[int,int] = (windowsize[0]//2-self.width//2,self.margin)
-        self.texture:pygame.Surface = pygame.Surface((self.width,self.height))
-        self.logicModule:GameLogic = GameLogic(self.width,self.height)
+        # the panel sprite has a margin of 20 and a width of 340
+        # 20/340 = margin/windowsize[0]
+        self.margin:float = ceil(windowsize[0]/17)
+        self.width:int = ceil(windowsize[0]-2*self.margin)
+        self.margin = ceil(self.margin)
+        self.texture:pygame.Surface = pygame.Surface((self.width,self.width))
+        self.logicModule:GameLogic = GameLogic(self.width,self.width)
+        print(self.width)
     def updatePanel(self, deltaTime:float)->None:
         self.logicModule.Update(deltaTime,self.texture)
     def showPanel(self, screen:pygame.Surface)->None:
-        screen.blit(self.texture,self.posOnScreen)
+        screen.blit(self.texture,(self.margin,self.margin))
