f7205ee05728278b5d0c64bd3125948ada5b534c
GamerIcePlayBence <gamerbence90@gmail.com>
2026-03-14 18:35:11 +0100

    Create time display and small fix

--- src/GAME/HUD.py
+++ src/GAME/HUD.py
@@ -13,6 +13,7 @@ class HUD:
         self.bluematerial:int = 0
         self.greenmaterial:int = 0
         self.yellowmaterial:int = 0
+        self.time:int = 0 #half hours
     def show(self, screen:pygame.Surface)->None:
         # Aspect ratio is handled already so we dont need to check the height
         screen.blit(self.img,(0,0))
@@ -21,25 +22,31 @@ class HUD:
         oresrect = pygame.Rect(0,0, 64, 64)
         oreswidth:int = 64
         fontcolor = (150, 150, 150)
-        # Full screen: self.leftmargin, self.topmargin
+        panelcolors = (50, 50, 50)
+
         pygame.draw.rect(screen, (70, 70, 70), pygame.Rect(self.leftmargin, self.topmargin, self.width, self.height), border_radius=5)
-        pygame.draw.rect(screen, (50, 50, 50), pygame.Rect(self.leftmargin+10, self.topmargin+5, 100, self.height-10), border_radius=5)
+        pygame.draw.rect(screen, panelcolors, pygame.Rect(self.leftmargin+10, self.topmargin+5, 120, self.height-10), border_radius=5)
         
-        #Kék anyag kirajzolása
+        #Blue material drawing
         oresrect.x = oreswidth
         screen.blit(oresimg, (self.leftmargin+20, self.topmargin+9), oresrect)
         bluematerialtxt = font.render(str(self.bluematerial), True, fontcolor)
         screen.blit(bluematerialtxt, ((self.leftmargin+20)+65, (self.topmargin+10)+15))
 
-        #Zöld anyag kirajzolása
+        #Green material drawing
         oresrect.x = int(oreswidth*3)
         screen.blit(oresimg, (self.leftmargin+20, self.topmargin+60), oresrect)
         greenmaterialtxt = font.render(str(self.greenmaterial), True, fontcolor)
         screen.blit(greenmaterialtxt, ((self.leftmargin+20)+65, (self.topmargin+60)+15))
 
-        #Sárga anyag kirajzolása
+        #Yellow material drawing
         oresrect.x = int(oreswidth*2)
         screen.blit(oresimg, (self.leftmargin+20, self.topmargin+120), oresrect)
         yellowmaterialtxt = font.render(str(self.yellowmaterial), True, fontcolor)
         screen.blit(yellowmaterialtxt, ((self.leftmargin+20)+65, (self.topmargin+120)+15))
 
+        #Time
+        pygame.draw.rect(screen, panelcolors, pygame.Rect(self.leftmargin+150, self.topmargin+5, self.width-300, 40), border_radius=5)
+
+        timetxt = font.render(f"{self.time//60:02d}:{self.time%60:02d}", True, fontcolor)
+        screen.blit(timetxt, ((self.leftmargin+150)+10, (self.topmargin+5)+10))
