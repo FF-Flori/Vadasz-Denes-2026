30e0574d9930e505d924866798ca3b61e2246380
FF-Flori <ffilkohazi3@gmail.com>
2026-03-16 18:05:28 +0100

    Added the main menu button (i think)

--- src/GAME/mainmenu.py
+++ src/GAME/mainmenu.py
@@ -5,12 +5,23 @@ class MainMenu:
     def __init__(self,windowsize:tuple[int,int])->None:
         self.texture:pygame.Surface = pygame.Surface(windowsize)
         self.texture.fill((100,125,255))
-        self.buttonPos:list[int] = [100,100]
-        self.button:pygame.Surface = pygame.Surface((200,40))
-    def update(self,gameHandler)->None:
+        self.width:int = windowsize[0]
+        self.height:int = windowsize[1]
+        self.buttonwidth:int = int(windowsize[0]*0.5) 
+        self.buttonheight:int = int(self.buttonwidth/3)
+        self.buttonPos:list[int] = [(windowsize[0]-self.buttonwidth)//2,windowsize[1]//2]
+        self.button:pygame.Surface = pygame.Surface((self.buttonwidth,self.buttonheight))
+        self.normButton:tuple[float,float,float,float] = (self.buttonPos[0]/self.width,self.buttonPos[1]/self.height,self.buttonwidth/self.width,self.buttonheight/self.height)
+        self.button.fill((0,0,0))
+    def update(self,gameHandler,windowDimensions:tuple[int,int,int,int])->None:
         if pygame.mouse.get_pressed(3)[0]:
             pos = pygame.mouse.get_pos()
-            if pos[0] >= self.buttonPos[0] and pos[0] <= self.button.get_width()+self.buttonPos[0] and pos[1] >= self.buttonPos[1] and pos[1] <= self.button.get_height()+self.buttonPos[1]:
+            pos = (pos[0]-windowDimensions[0],pos[1]-windowDimensions[1])
+            normalized:tuple[float,float] = (pos[0]/windowDimensions[2],pos[1]/windowDimensions[3])
+            if normalized[0] >= self.normButton[0] and \
+               normalized[0] <= self.normButton[0]+self.normButton[2] and \
+               normalized[1] >= self.normButton[1] and \
+               normalized[1] <= self.normButton[1]+self.normButton[3]:
                 gameHandler.inMenu = False
     def show(self,screen:pygame.Surface)->None:
         screen.blit(self.texture,(0,0))
--- src/GUI/GameWindow.py
+++ src/GUI/GameWindow.py
@@ -4,6 +4,7 @@ from src.GAME.game import *
 class GameWindow:
     def __init__(self,width:int,height:int) -> None:
         self.texture:pygame.Surface = pygame.Surface((width,height))
+        self.cached:pygame.Surface = self.texture
         self.width:int = width
         self.height:int = height
         self.aspectRatio:float = width/height
@@ -12,16 +13,27 @@ class GameWindow:
         screenwidth:int = screen.get_width()
         screenheight:int = screen.get_height()
 
-        self.gameHandler.drawGame(self.texture,deltaTime)
+        xpos:int = 0
+        ypos:int = 0
+        newwidth:int = 0
+        newheight:int = 0
+        if self.aspectRatio > screenwidth/screenheight:
+            newwidth = screenwidth
+            newheight = int(screenwidth/self.aspectRatio)
+            xpos = 0
+            ypos = (screenheight-newheight)//2
+        elif self.aspectRatio < screenwidth/screenheight:
+            newwidth = int(self.aspectRatio*screenheight)
+            newheight = screenheight
+            xpos = (screenwidth-newwidth)//2
+        else:
+            newwidth = screenwidth
+            newheight = screenheight
+
+
+        self.gameHandler.drawGame(self.texture,deltaTime,(xpos,ypos,newwidth,newheight))
+
+
+        screen.blit(pygame.transform.scale(self.texture,(newwidth,newheight)),(xpos,ypos))
 
         # The width is bigger than normal relative to the height
-        if self.aspectRatio > screenwidth/screenheight:
-            newwidth:int = screenwidth
-            newheight:int = int(screenwidth/self.aspectRatio)
-            screen.blit(pygame.transform.scale(self.texture,(newwidth,newheight)),(0,(screenheight-newheight)/2))
-        elif self.aspectRatio < screenwidth/screenheight:
-            newwidth:int = int(self.aspectRatio*screenheight)
-            newheight:int = screenheight
-            screen.blit(pygame.transform.scale(self.texture,(newwidth,newheight)),((screenwidth-newwidth)/2,0))
-        else:
-            screen.blit(pygame.transform.scale(self.texture,(screenwidth,screenheight)),(0,0))
